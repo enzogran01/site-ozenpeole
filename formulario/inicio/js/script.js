cfc3789fcdde6186dd6a776339441e55eac30cb4
unknown <aluno@p0704.etecaf.local>
2024-08-30 15:33:02 -0300

    formulario addition

---
+++ formulario/inicio/js/script.js
@@ -0,0 +1,7 @@
+let selecione = document.querySelector("#selecione")
+//const form = document.getElementById("form")
+//area = document.querySelector
+function Onselecione(event){
+    console.log(event.target.value);
+
+}
