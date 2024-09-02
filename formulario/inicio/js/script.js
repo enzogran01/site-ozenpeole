b1123a75c2df8aefda7353cf89b2beee497d033f
unknown <aluno@METAL11.etecaf.local>
2024-09-02 16:48:50 -0300

    inicio-form

--- formulario/inicio/js/script.js
+++ formulario/inicio/js/script.js
@@ -1,7 +1,14 @@
-let selecione = document.querySelector("#selecione")
-//const form = document.getElementById("form")
-//area = document.querySelector
-function Onselecione(event){
-    console.log(event.target.value);
+let selecione = document.querySelector("#selecione");
+let materiais = document.getElementsByTagName("materias")[0].value;
+let roupas = document.getElementsByTagName("roupas")[1].value;
+let restaurante = document.getElementsByTagName("restaurante")[2].value;
+let informatica = document.getElementsByTagName("informatica")[3].value;
 
+const OnClick = 0;
+
+function OnClick(event){
+   selecione.getElementById("comercios");
+   if(selecione == materais){
+       href = "./construção/construção.html";
+   }
 }
