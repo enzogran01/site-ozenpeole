b70761d8a216a9473ad18e35f711b459f032ca9d
unknown <aluno@AUTO04.etecaf.local>
2024-09-05 17:48:24 -0300

    respostas variáveis

--- formulario/inicio/js/script.js
+++ formulario/inicio/js/script.js
@@ -3,9 +3,6 @@ let materiais = "construcao.html";
 let roupas = "loja-roupa.html"
 let restaurante = "restaurante.html";
 let informatica = "informatica.html"
-
-
-
 // Adiciona um event listener ao botão para capturar o valor selecionado na dropdown
 document.getElementById('confirmar').addEventListener('click', function() {
     // Captura o valor selecionado na dropdown
@@ -25,3 +22,29 @@ document.getElementById('confirmar').addEventListener('click', function() {
         window.location.href = '../informatica/informatica.html';
     }
 });
+
+
+//configuração radios
+function obterResposta(){
+    //entrar no form
+    let form1 = document.getElementById("resposta1");
+    //querySelector para selecionar o radio
+    let resposta1Selecionada = form1.querySelector('input[name = "idade"]:checked');
+
+    if(resposta1Selecionada){
+        //pegando o valor do radio
+        let resposta1 = resposta1Selecionada.value;
+    
+    //função está acabando com o dado na variável resposta1
+
+    console.log('Resposta selecionada: ' + resposta1);
+    alert(resposta1Selecionada.value);
+    }
+    
+    else{
+        console.log('Nenhuma resposta selecionada')
+    }
+
+}
+// Adicione um listener para o evento de clique no botão
+document.getElementById('confirmar2').addEventListener('click', obterResposta);
