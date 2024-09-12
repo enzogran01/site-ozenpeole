5afd40104212c85813910cc50e56220893d576a9
LeoOliveiraGIT <leonardo.oliveira740@etec.sp.gov.br>
2024-09-11 23:14:28 -0300

    popUP

---
+++ criarcampanha/scripts.js
@@ -0,0 +1,4 @@
+const openModalButton = document.querySelector("#open-modal");
+const closemodalbutton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
