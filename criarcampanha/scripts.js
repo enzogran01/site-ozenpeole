b85407de7b717a0f7fb43bbd4c72f767164915cf
unknown <aluno@MEC07.etecaf.local>
2024-09-24 14:50:51 -0300

    exclusão-pasta errada

--- criarcampanha/scripts.js
+++
@@ -1,4 +0,0 @@
-const openModalButton = document.querySelector("#open-modal");
-const closemodalbutton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
