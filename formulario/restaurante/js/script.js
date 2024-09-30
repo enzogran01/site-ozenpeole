b267047dc261c371f765e65a5deb2dfa4e1a3f4f
unknown <aluno@AUTO04.etecaf.local>
2024-09-30 14:17:28 -0300

    formulario-edit

--- formulario/restaurante/js/script.js
+++ formulario/restaurante/js/script.js
@@ -12,10 +12,6 @@ function btnVoltar(voltar) {
     window.location.href="../inicio/formulario.html";
 }
 
-document.getElementById('voltar').addEventListener('click', btnVoltar);
-
-
-
 
 
 //configuração radios
@@ -67,16 +63,87 @@ function clickGeral(btnGeral){
     console.log("Propaganda:", propaganda);
 
 
-    alert(`Suas respostas foram: 
-        \nIdade: ${idade}
-        \nLocal: ${local}
-        \nRede social: ${social}
-        \nMaioria das vendas: ${venda}
-        \nFaixa de preço: ${preco}
-        \nJá faz propaganda? ${propaganda}
-        \nCampanha: ${campanha}
-        \nDados geral: ${geral}`);
+    // alert(`Suas respostas foram: 
+    //     \nIdade: ${idade}
+    //     \nLocal: ${local}
+    //     \nRede social: ${social}
+    //     \nMaioria das vendas: ${venda}
+    //     \nFaixa de preço: ${preco}
+    //     \nJá faz propaganda? ${propaganda}
+    //     \nCampanha: ${campanha}
+    //     \nDados geral: ${geral}`);
+
+
+
+        //função para exibir uma campanha de demonstração
+        document.getElementById('campanha').style.display = 'block';
+        for(let i = 1; i <= totalPerguntas; i++){
+        document.getElementById(`box-pergunta${i}`).style.display = 'none';
+    }
 }
 
-// Adicione um listener para o evento de clique no botão
-document.getElementById('btngeral').addEventListener('click', btnkGeral);
+
+
+
+let perguntaAtual = 1;
+const totalPerguntas = 6;
+
+//função pra passar pergunta
+function proximaPergunta(){
+    //oculta todas as perguntas
+    for(let i = 1; i <= totalPerguntas; i++){
+        document.getElementById(`box-pergunta${i}`).style.display = 'none';
+    }
+
+    //tirar o botão quando chegar n aúltima pergunta
+        if(perguntaAtual === totalPerguntas){
+            document.getElementById('btnAvancar').style.display = 'none';
+        }else{
+            document.getElementById('btnAvancar').style.display = 'block';
+        }
+
+    //mostra o botão de pergunta anterios só quando aparece a segunda pergunta
+    if(perguntaAtual === 1){
+        document.getElementById('btnAnterior').style.display = 'none'
+    }else{
+        document.getElementById('btnAnterior').style.display = 'block'
+    }
+
+    //mostra o botão de confirmar só quando chega a última pergunta
+    if(perguntaAtual != totalPerguntas){
+        document.getElementById('btnGeral').style.display = 'none';
+    }else{
+        document.getElementById('btnGeral').style.display = 'block';
+    }
+    
+    //mostra a pergunta atual com base no id
+    document.getElementById(`box-pergunta${perguntaAtual}`).style.display = 'block';
+
+    //atualiza os botões
+    document.getElementById('btnAnterior').disabled = perguntaAtual === 1;
+    document.getElementById('btnAvancar').disabled = perguntaAtual === totalPerguntas;
+}
+
+//função para avançar a pergunta
+function avancar(){
+    if(perguntaAtual < totalPerguntas){
+        perguntaAtual++;
+        proximaPergunta();
+    } 
+}
+
+//função para volta pergunta
+function anterior(){
+    if(perguntaAtual > 1){
+        perguntaAtual--;
+        proximaPergunta();
+        document.getElementById('campanha').style.display = 'none';
+    } 
+}
+
+//inicia a primeira pergunta
+proximaPergunta();
+
+//adiciona o click aos botões
+document.getElementById('btnAvancar').addEventListener('click', avancar);
+document.getElementById('btnAnterior').addEventListener('click', anterior);
